7c3987f029da01fadc707f7bfb43581e314107b9
Mikhail Orlov <mix.djoker@gmail.com>
2020-04-19 17:50:31 +1000

    JS Function 5_array completed

---
+++ src/js_functions/5_array.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-restricted-syntax */
+// @ts-check
+import _ from 'lodash';
+
+/**
+ * @param {any[]} arrays
+ */
+const union = (...arrays) => {
+  const resultArray = [];
+  for (const array of arrays) {
+    const uniqElementArray = _.uniq(array);
+    resultArray.push(...uniqElementArray);
+  }
+  return _.uniq(resultArray);
+};
+
+export default union;
---
+++ test/5_array.js
@@ -0,0 +1,5 @@
+import union from '../src/js_functions/5_array.js';
+
+console.log(union([3]));
+console.log(union([3, 2], [2, 2, 1]));
+console.log(union(['a', 3, false], [true, false, 3], [false, 5, 8]));
